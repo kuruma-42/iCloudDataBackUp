7b3fb45d7f47fc183588fe4cc7e6985fdbc7d9db
yongjuncha <perfect_yon@naver.com>
2022-02-03 10:54:33 +0900

    Initial Commit

---
+++ iCloudDataBackUp/iCloudDataBackUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCloudDataBackUpApp.swift
+//  iCloudDataBackUp
+//
+//  Created by Yong Jun Cha on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct iCloudDataBackUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
